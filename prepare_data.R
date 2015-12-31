92a78975c125f08f15dcdbeb2e852354a076495b
Abby Mullen <abby@lincolnmullen.com>
2015-12-31 11:08:54 -0500

    Prepare data

---
+++ prepare_data.R
@@ -0,0 +1,14 @@
+library(purrr)
+library(readr)
+library(dplyr)
+library(googlesheets)
+
+"1-O0sCZWGkSNFK7TkzB56yD6hED_bNsplWZ1m6BjxFHE" %>%
+  gs_key() %>% 
+  gs_download(ws = "Master", to = "ship-movements.csv",
+              overwrite = TRUE)
+
+raw <- read_csv("ship-movements.csv")
+raw <- raw %>% 
+  filter(!is.na(long),
+         !is.na(lat))
